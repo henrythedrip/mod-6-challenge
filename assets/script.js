93245217b8724c30d8016bff9eb436090fe95ea4
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2024-01-24 15:05:44 -0700

    completed the functionality and styling of the webpage. ready for deployment.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-console.log('Hello from script.js');
-
 // example weather api url
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // 55c06a5306f77f20b1fd135618cf25af
@@ -7,108 +5,234 @@ console.log('Hello from script.js');
 // example geocoding api link
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-
-
 // TODO: this shouldnt be done, but we have no backend so we must put it here
 const API_KEY = '55c06a5306f77f20b1fd135618cf25af'
 
-
-// search history should be saved in local storage, would be nice to save location data as well as names
-
 // get elemements on page
 const currentWeatherContainer = document.getElementById('display-container');
+const fiveDayforecastContainer = document.getElementById('five-day-forecast-container');
 const searchHistory = document.getElementById('search-history');
 const searchButton = document.getElementById('search-submit');
+const clearLocalStorageButton = document.getElementById('clear-local-storage');
 
 // handle searching for a location
-searchButton.addEventListener('click', async event => {
-    const cityValue = document.getElementById('city-search-box').value;
-    // console.log(cityValue);
+// searchButton.addEventListener('click', latitudeAndLongitude)
+searchButton.addEventListener('click', makeWeatherWork)
+clearLocalStorageButton.addEventListener('click', () => {
+  localStorage.clear();
+})
 
-    const locationResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${API_KEY}`);
-    const locationJSON = await locationResponse.json()
-    console.log(locationJSON);
+// handle reading the history on initial page load
+function paintSearchHistory() {
+  // clear out the container
+  while (searchHistory.firstChild) {
+    searchHistory.removeChild(searchHistory.lastChild)
+  }
 
-    // handle zero locations found/bad search
-    if (locationJSON.length == 0) {
-        alert('Find a real place friend!')
-    }
-
-    // location is an object {}
-    const location = locationJSON[0];
-    const lat = location.lat;
-    const lon = location.lon;
-    console.log(lat, lon);
-
-    // get weather data
-    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-    const weatherJson = await weatherResponse.json()
-    console.log(weatherJson);
-
-    // we have a location, so lets add it to the history
-    const temp = {
-      name: weatherJson.city.name,
-      coord: weatherJson.city.coord,
-      // timestamp so we can delete oldest history
-      timestamp: Date.now()
-    }
-    
-    // TODO: get rid of oldest history if there are too many
-    // if (localStorage.length >= 10) {
-    //   // sort all localstorage entries by timestamp
-    //   // remove oldest one
-    //   const sorted = Array(localStorage).sort((a, b) => {
-    //     console.log(a);
-    //     console.log(b);
-    //     return a.timestamp - b.timestamp;
-    //  });
-    //  console.log(sorted);
-    //  localStorage.removeItem(sorted[9]);
-    // }
-
-    localStorage.setItem(String(weatherJson.city.id), JSON.stringify(temp))
-
-    // handle adding location to search history
+  // iterate over items in localstorage
+  Object.values(localStorage).forEach(item => {
+    // item starts out as string from localstorage
+    item = JSON.parse(item);
     let newLocation = document.createElement('div');
     newLocation.setAttribute('class', 'location-in-history')
     let cityNameEl = document.createElement('p')
-    cityNameEl.textContent = weatherJson.city.name
+    cityNameEl.textContent = item.name
     newLocation.appendChild(cityNameEl)
+    
+    // add event listener to history item
+    newLocation.addEventListener('click', async event => {
+      const weatherData = await getCurrentWeather(item.coord);
+      paintCurrentWeather(weatherData);
+      
+      const fiveDay = await getFiveDayWeather(item.coord);
+      paintFiveDayWeather(fiveDay);
+    })
+    
+    // add location to search history container
     searchHistory.appendChild(newLocation)
-
-})
-
-// we probably want the weather object for the location to render as an argument
-function renderWeatherData(weatherObject) {
-  console.log('rendering weather data');
-
+  })
 }
 
-// TODO: create function to render localstorage history locations onto the screen for initial pageload and for when we change localstorage
+// call on initial page load to populate history
+paintSearchHistory();
 
-// TODO: create function to draw current and 5 day forecast for a location on the screen, so we can do it when we search and when we click on something in the history
-function drawWeather() {
-  // // handle adding current weather to page
-  // let current = document.createElement('div')
-  // current.setAttribute('class', 'current-days-weather')
-  // current.appendChild(cityNameEl)
+// get the lat/long for a location based on its name
+async function getLatitudeAndLongitude() {
+  // https://openweathermap.org/api/geocoding-api
 
-  // console.log(weatherJson);
-  
-  // // date
-  // let currentDate = document.createElement('p')
-  
-  // // icon
-  // let currentIcon = document.createElement('p')
-  
-  // // temperature
-  // let currentTemp = document.createElement('p')
-  
-  // // humidity
-  // let currentHumidity = document.createElement('p')
-  
-  // // wind speed
-  // let currentWindSpeed = document.createElement('p')
-  
-  // currentWeatherContainer.appendChild(current)
+  const cityValue = document.getElementById('city-search-box').value;
+
+  const url = 'https://api.openweathermap.org/geo/1.0/direct?' +
+    `q=${cityValue}` +  // need to pass city name
+    '&limit=1' +        // we only want one result
+    `&appid=${API_KEY}` // need to pass api key
+
+  const locationResponse = await fetch(url);
+  const locationJSON = await locationResponse.json()
+
+  // handle zero locations found/bad search
+  if (locationJSON.length == 0) {
+    alert('Find a real place friend!')
+    return null;
+  }
+
+  // location is an object {}
+  return locationJSON[0];
+}
+
+// get current weather data for a location
+async function getCurrentWeather(location) {
+  // https://openweathermap.org/current
+
+  // get weather data
+  const url = 'https://api.openweathermap.org/data/2.5/weather' +
+    `?lat=${location.lat}` +     // need to provide location
+    `&lon=${location.lon}` +     // need to provide location
+    `&appid=${API_KEY}`          // need to provide api key
+
+  const weatherResponse = await fetch(url);
+  const weatherJson = await weatherResponse.json();
+  return weatherJson;
+}
+
+// get the five day forecast
+async function getFiveDayWeather(location) {
+  // https://openweathermap.org/forecast5
+
+  // get weather data
+  const url = 'https://api.openweathermap.org/data/2.5/forecast' +
+    '?units=imperial' +          // need to make units ferinheight
+    `&lat=${location.lat}` +     // need to provide location
+    `&lon=${location.lon}` +     // need to provide location
+    `&appid=${API_KEY}`          // need to provide api key
+
+  const weatherResponse = await fetch(url);
+  const weatherJson = await weatherResponse.json();
+
+  // allows only indecies at noon to be pushed into a new array and returns it
+  let dayArray = [];
+  for (let i = 4; i < weatherJson.list.length; i += 8) {
+    dayArray.push(weatherJson.list[i]);
+  }
+  return dayArray;
+}
+
+// paint each of the five weather forecasts
+function paintFiveDayWeather(dayArray) {
+  // clear out the container
+  while (fiveDayforecastContainer.firstChild) {
+    fiveDayforecastContainer.removeChild(fiveDayforecastContainer.lastChild)
+  }
+
+  dayArray.forEach(singleDay => {
+    // console.log(singleDay);
+    let todaysCard = document.createElement('div');
+    todaysCard.setAttribute('class', 'five-day-card');
+
+    //date
+    let fiveDayDate = document.createElement('p');
+    let date = new Date(singleDay.dt_txt)
+    fiveDayDate.textContent = date.toDateString()
+    todaysCard.appendChild(fiveDayDate)
+
+    //icon
+    let fiveDayIcon = document.createElement('img')
+    fiveDayIcon.setAttribute('src', `https://openweathermap.org/img/wn/${singleDay.weather[0].icon}@2x.png`)
+    todaysCard.appendChild(fiveDayIcon)
+
+    //wind
+    let fiveDayWind = document.createElement('p');
+    fiveDayWind.textContent = `Wind speed: ${singleDay.wind.speed}`;
+    todaysCard.appendChild(fiveDayWind)
+
+    //temp
+    let fiveDayTemp = document.createElement('p');
+    fiveDayTemp.textContent = `Temperature: ${singleDay.main.temp} F`
+    todaysCard.appendChild(fiveDayTemp)
+
+    //humidity
+    let fiveDayHumidity = document.createElement('p');
+    fiveDayHumidity.textContent = `Humidity: ${singleDay.main.humidity}%`
+    todaysCard.appendChild(fiveDayHumidity)
+
+    //description
+    let fiveDayDescription = document.createElement('p');
+    fiveDayDescription.textContent = `Weather: ${singleDay.weather[0].description}`
+    todaysCard.appendChild(fiveDayDescription)
+
+    fiveDayforecastContainer.appendChild(todaysCard);
+  })
+}
+
+
+function paintCurrentWeather(weatherObj) {
+  // clear out the container
+  while (currentWeatherContainer.firstChild) {
+    currentWeatherContainer.removeChild(currentWeatherContainer.lastChild)
+  }
+
+  // name
+  let currentLocationName = document.createElement('p');
+  currentLocationName.textContent = weatherObj.name
+  currentWeatherContainer.appendChild(currentLocationName)
+
+  //date
+  let currentDate = document.createElement('p')
+  currentDate.textContent = `Date: ${new Date().toDateString()}`
+  currentWeatherContainer.appendChild(currentDate)
+
+  //icon
+  let currentIcon = document.createElement('img')
+  currentIcon.setAttribute('src', `https://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png`)
+  currentWeatherContainer.appendChild(currentIcon)
+
+  //wind
+  let currentWindSpeed = document.createElement('p')
+  currentWindSpeed.textContent = `Wind speed: ${weatherObj.wind.speed}`
+  currentWeatherContainer.appendChild(currentWindSpeed);
+
+  //temp
+  let currentTemperature = document.createElement('p')
+  // the api returns temperatures in Kelvin, convert to fahrenheit
+  let fahr = ((weatherObj.main.temp - 273.15) * 1.8) + 32
+  currentTemperature.textContent = `Temperature: ${fahr.toFixed(1)} F`
+  currentWeatherContainer.appendChild(currentTemperature)
+
+  //humidity
+  let currentHumidity = document.createElement('p')
+  currentHumidity.textContent = `Humidity: ${weatherObj.main.humidity}%`
+  currentWeatherContainer.appendChild(currentHumidity)
+
+  //description (snowy)
+  let currentWeatherDescription = document.createElement('p')
+  currentWeatherDescription.textContent = `Weather: ${weatherObj.weather[0].description}`
+  currentWeatherContainer.appendChild(currentWeatherDescription)
+}
+
+
+async function makeWeatherWork() {
+
+  const location = await getLatitudeAndLongitude()
+  if (location === null) return;
+
+  const weatherData = await getCurrentWeather(location);
+  paintCurrentWeather(weatherData);
+
+  const fiveDayWeatherData = await getFiveDayWeather(location);
+  paintFiveDayWeather(fiveDayWeatherData);
+
+  // we have a location, so lets add it to the history
+  const temp = {
+    name: location.name,
+    coord: weatherData.coord,
+    // timestamp so we can delete oldest history
+    timestamp: Date.now()
+  }
+
+  // if its a new location, add to history and localstorage
+  if (!Object.keys(localStorage).includes(String(weatherData.id))) {
+    // add new location to localstorage and then repaint the history
+    localStorage.setItem(String(weatherData.id), JSON.stringify(temp))
+    paintSearchHistory();
+  }
 }
